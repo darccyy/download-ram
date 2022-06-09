2edc4fe521314ab48c3c259f7733dfa3e3cda5a3
DarccMan <darcy3057@gmail.com>
2022-06-09 10:21:54 +1000

    Add download

--- script.js
+++ script.js
@@ -1,23 +1,39 @@
-var comments = [
+console.log("This is [not] a Scam!");
+var min = 3, // Minimum ram value
+  max = 8, // Maximum ram value
+  comments = [
+    // Description of ram value
     "God awful!",
     "Terrible!",
     "Real bad",
     "Bad",
     "Not good",
     "Decent",
-  ],
-  min = 3,
-  max = 8;
+  ];
 
+// Run test on button click, load
 async function test() {
   $("#display").text("Loading...");
   await F.sleep(800);
   var ram = getRAM();
-  $("#display").text(
-    `${ram}GB (${comments[Math.floor(((ram - min) / (max - min)) * comments.length)] || "idk"})`,
-  );
+  $("#display").text(`${ram}GB (${getComment(ram)})`);
+
+  showDownload();
 }
 
+// Unhide download link
+function showDownload() {
+  $("#download").attr("show", true);
+}
+
+// Get comment from ram value
+function getComment(ram) {
+  return comments[
+    Math.floor(((ram - min) / (max - min)) * (comments.length - 1))
+  ];
+}
+
+// Get ram value from fingerprint
 function getRAM() {
   return F.round(
     new Math.seedrandom(getFingerprint())() * (max - min) + min,
@@ -25,11 +41,8 @@ function getRAM() {
   );
 }
 
+// Get fingerprint from user data
 function getFingerprint() {
-  // return new Array(20)
-  //   .fill(null)
-  //   .map(i => "abcdefghijklmnopqrstuvwxyz"[Math.floor(Math.random() * 26)])
-  //   .join("");
   return (
     navigator.userAgent +
     navigator.buildID +
